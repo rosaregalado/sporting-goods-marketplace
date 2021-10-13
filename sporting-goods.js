d1308ac9c79c81c00fc4dde24d9f57de964ed361
Rosa Regalado <rosaregalado101@gmail.com>
2021-10-13 16:45:28 -0700

    latest version of website

--- sporting-goods.js
+++ sporting-goods.js
@@ -1,4 +1,4 @@
-import inventory from '../inventory.js';
+import inventory from './inventory.js';
 const displaySaleItems = document.querySelector('#items');
 
 
@@ -31,13 +31,41 @@ for (let i = 0; i < inventory.length; i++) {
   price.style.color = "red";
   price.style.fontSize = "30px";
 
+  
+  // const button = document.createElement('button');
+  // button.id = inventory[i].name;
+  // button.innerHTML = "Contact Seller";
+  // container.appendChild(button);
+  
+  
+  
   //create contact-seller button
-  const button = document.createElement('button');
-  button.setAttribute("id", "contact-button");
-  button.id = inventory[i].name;
-  button.innerHTML = "Contact Seller";
-  container.appendChild(button);
-  //style contact-seller button
+  const contact = document.createElement("button");
+  contact.id = inventory[i].name;
+  contact.innerHTML = "Contact Seller";
+  container.appendChild(contact);
+
+  //event listener for button
+  contact.onclick = function() {
+    
+    var contactPopup = document.getElementById("contact-popup"),
+    dimScreen = document.createElement("div");
+    
+    dimScreen.style.width =  window.innerWidth + 'px';
+    dimScreen.style.height = window.innerHeight + 'px';
+    dimScreen.className = 'dimScreen';
+    
+    dimScreen.onclick = function() {
+      document.body.removeChild(this);   
+      contactPopup.style.visibility = 'hidden';
+    }
+    
+    document.body.appendChild(dimScreen);
+    
+    contactPopup.style.visibility = 'visible';
+    
+    return false;
+  }
+  
   
 }
-
